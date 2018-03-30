695a30aa029e47473dcd7be8c178a8391192a686
cheneychen <cheneychen@YANGPC>
2018-03-30 13:47:27 +0800

    Java类加载机制

---
+++ ThinkInJavaTest/src/com/yangwu/util/ClassLoading.java
@@ -0,0 +1,15 @@
+package com.yangwu.util;
+
+public class ClassLoading {
+
+
+	public ClassLoading() {
+		System.out.println("父类的构造方法");
+	}
+	
+	{System.out.println("父类的成员变量");}
+	
+	static {System.out.println("父类的静态成员变量");}
+	
+
+}
