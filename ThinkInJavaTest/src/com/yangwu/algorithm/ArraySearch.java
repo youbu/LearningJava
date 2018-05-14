7904d8d15253c93665487db065ec307c8658e566
youbu <549047879@qq.com>
2018-05-14 22:42:28 +0800

    aa

---
+++ ThinkInJavaTest/src/com/yangwu/algorithm/ArraySearch.java
@@ -0,0 +1,37 @@
+package com.yangwu.algorithm;
+
+import org.apache.tools.ant.taskdefs.optional.jlink.jlink;
+
+/**
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序， 每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * 
+ * @author Administrator
+ *
+ */
+public class ArraySearch {
+
+	public static void main(String[] args) {
+		int[][] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+		int target = 56;
+		System.out.println( find(target, array));
+	}
+
+	public static boolean find(int target, int[][] array) {
+		int i = array.length - 1;
+		int colNum = array[0].length;
+		int j = 0;
+		while (i > 0 && j < colNum) {
+			if (array[i][j]>target) {
+				i--;
+			}else if (array[i][j]<target) {
+				j++;
+			}else {
+				System.out.println("array["+ i +"]"+"["+j+"] = " + target);
+				return true;
+			}
+			
+		}
+		return false;
+	}
+}
